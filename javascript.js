b83eca73ca6b0dd17c38998572c278dcc7f79f72
Domingo Cabeza <domingoacd1@gmail.com>
2019-01-07 20:42:38 -0400

    Modified variables and functions names to make it more readable. Changed font family, header padding and  buttons left margin.

--- javascript.js
+++ javascript.js
@@ -2,94 +2,95 @@ let tempInterval;
 /**
  * Shows/hides the "How does it work?" panel. Assigns events to the open and close buttons.
  */
-function openClose(){
-    let openButton = document.getElementById("info-open"),
-        infoPanel= document.getElementById("info"),
-        closeButton = document.getElementById("info-close");
-    openButton.addEventListener("click", ()=>{
-        infoPanel.classList.toggle("info-show");
+function openCloseFaq() {
+    const openFaqButton = document.getElementById("info-open");
+    const faqTextPanel = document.getElementById("info");
+    const closeFaqButton = document.getElementById("info-close");
+    openFaqButton.addEventListener("click", () => {
+        faqTextPanel.classList.toggle("info-show");
     });
-    closeButton.addEventListener("click", ()=>{
-        infoPanel.classList.toggle("info-show");
+    closeFaqButton.addEventListener("click", () => {
+        faqTextPanel.classList.toggle("info-show");
     });
 }
 /**
  * Activates the notifications when the temp cycle ends 
  * @param {String} currentCycle -The cycle that is ending (rest, long rest or work)- 
  */
-function notifications(currentCycle){
-    let notificationSound = new Audio("notification.wav"), 
-    message;
+function notifications(currentCycle) {
+    const notificationSound = new Audio("notification.wav");
+    let notificationMessage;
     switch (currentCycle) {
         case "rest":
-            message = "Time to work!";
+            notificationMessage = "Time to work!";
             break;
-    
+
         case "longRest":
-            message = "Ok, to work again!";
+            notificationMessage = "Ok, to work again!";
             break;
-    
+
         case "work":
-            message = "Is time to rest!";
+            notificationMessage = "Is time to rest!";
             break;
-    
+
         default:
-            message = "Message error";
+            notificationMessage = "Message error";
             break;
     }
     notificationSound.play();
-    new Notification(message);
+    new Notification(notificationMessage);
 }
 /**
  * Writes the remaining time on screen 
  * @param {Number} minutes -Timer remaining minutes- 
  * @param {Number} seconds -Timer remaining seconds-
  */
-function outputTime(minutes, seconds){
-    let display = document.getElementById("time"),
-        output = (minutes < 10 ? "0" + minutes : minutes) + ":" + (seconds < 10 ? "0" + seconds : seconds);
-    display.innerText = output;
+function printTime(minutes, seconds) {
+    const timerDisplay = document.getElementById("time");
+    const timeToPrint = (minutes < 10 ? "0" + minutes : minutes) + ":" + (seconds < 10 ? "0" + seconds : seconds);
+    timerDisplay.innerText = timeToPrint;
 }
 /**
  * Calculates the remaining time of the timer
- *  @param {Number} time -The starting time (in minutes) of the timer-
+ *  @param {Number} totalTimeInMinutes -The starting time (in minutes) of the timer-
  *  @param {Boolean} intervalIsOn -Checks wheter an interval is currently running-
- *  @param {String} currentCycle -The interval that is going to run (rest, long rest or work)-
+ *  @param {String} selectedCycle -The interval that is going to run (rest, long rest or work)-
  */
-function inputTime(time, intervalIsOn, currentCycle){
-    let totalTimeSeconds = Date.now()/1000 + time*60,
-        seconds = -1;
+function startTimer(totalTimeInMinutes, intervalIsOn, selectedCycle) {
+    let totalTimeInSeconds = Date.now() / 1000 + totalTimeInMinutes * 60;
+    let secondsToPrint = -1;
+    clearInterval(tempInterval);
+    if (intervalIsOn) {
         clearInterval(tempInterval);
-        if(intervalIsOn){
+    }
+    tempInterval = setInterval(() => {
+        let remainingTime = Math.floor(totalTimeInSeconds - Date.now() / 1000);
+        if (remainingTime < -1) {
+            notifications(selectedCycle);
             clearInterval(tempInterval);
-        }
-        tempInterval = setInterval(()=>{
-            let tiempoRestante = Math.floor(totalTimeSeconds - Date.now()/1000);
-            if(tiempoRestante < -1){
-                notifications(currentCycle);
-                clearInterval(tempInterval);
-
-            }else{
-                if(seconds <= 0){
-                    time--;
-                    seconds = 60;
-                }
-                seconds--;
-                outputTime(time, seconds);
+        } else {
+            if (secondsToPrint <= 0) {
+                totalTimeInMinutes--;
+                secondsToPrint = 60;
             }
-        },1000);
+            secondsToPrint--;
+            printTime(totalTimeInMinutes, secondsToPrint);
+        }
+    }, 1000);
 }
 /**
  * Notification request. Assigns events to buttons. Starts the timer. 
  */
-function start(){
-    let buttonsList = document.getElementsByClassName("temp-selector");
-    for(let button of buttonsList){
-        button.addEventListener("click", ()=>{
-            inputTime(Number(button.value), status, button.getAttribute("data-description"));
+function start() {
+    const tempButtons = document.getElementsByClassName("temp-selector");
+    for (let button of tempButtons) {
+        button.addEventListener("click", () => {
+            const timeToWait = Number(button.value);
+            const selectedCycle = button.getAttribute("data-description");
+            startTimer(timeToWait, status, selectedCycle);
         });
     }
-    openClose();
+    openCloseFaq();
     Notification.requestPermission();
 }
-start();
+window.onload = start;
